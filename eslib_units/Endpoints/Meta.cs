b351ac877d43ddd621367d572dbf0fd7dbb5f78d
Kieran Sunshine <ksunshine@visiumkms.com>
2021-01-02 16:34:47 +0000

    Added basic Meta endpoint unit test

---
+++ eslib_units/Endpoints/Meta.cs
@@ -0,0 +1,31 @@
+﻿using eslib.Endpoints;
+using eslib.Models;
+using eslib.Services;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace eslib_units.Endpoints
+{
+    public class MetaTests
+    {
+        [Test]
+        public void Ping()
+        {
+            var mock = new Mock<IDataService>();
+
+            var mockResponse = Task.FromResult(new Response<string>() { message = "ok" });
+
+            // Ensure that the call to fetch returns our mocked response.
+            mock.Setup(m => m.Fetch<string>(It.IsAny<string>()))
+                .Returns(mockResponse);
+
+            // Create our endpoint and call ping.
+            var metaEndpoint = new Meta(mock.Object);
+            var result = metaEndpoint.Ping();
+
+            // Assert that the outcome is what was expected.
+            Assert.AreEqual(result, mockResponse.Result.message);
+        }
+    }
+}
