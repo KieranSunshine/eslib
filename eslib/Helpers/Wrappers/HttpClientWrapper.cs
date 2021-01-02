85b23b3bda4244ae039588fd345d623f2f85df14
Kieran Sunshine <ksunshine@visiumkms.com>
2021-01-02 16:04:17 +0000

    Created a wrapper for HttpClient

---
+++ eslib/Helpers/Wrappers/HttpClientWrapper.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace eslib.Helpers.Wrappers
+{
+    internal class HttpClientWrapper : IHttpClientWrapper
+    {
+        private static readonly HttpClient _httpClient;
+
+        static HttpClientWrapper()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            return await _httpClient.GetAsync(url);
+        }
+
+        public void Dispose()
+        {
+            _httpClient?.Dispose();
+        }
+    }
+
+    public interface IHttpClientWrapper
+    {
+        public Task<HttpResponseMessage> GetAsync(string url);
+    }
+}
