d7eebc9b919c33834ec6fb79ce1a2921c06dfe11
Kieran Sunshine <ksunshine@visiumkms.com>
2021-01-02 16:15:23 +0000

    Created a basic data service and response model

---
+++ eslib/Models/Response.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eslib.Models
+{
+    public class Response<T>
+    {
+        public T data { get; set; }
+        public string message { get; set; }
+    }
+}
---
+++ eslib/Services/DataService.cs
@@ -0,0 +1,55 @@
+﻿using eslib.Helpers.Wrappers;
+using eslib.Models;
+using Microsoft.Extensions.Options;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace eslib.Services
+{
+    public class DataService : IDataService
+    {
+        private readonly IOptions<ApiOptions> _options;
+        private readonly IHttpClientWrapper _httpClient;
+
+        public DataService(IOptions<ApiOptions> options)
+        {
+            _options = options;
+            _httpClient = new HttpClientWrapper();
+        }
+
+        public DataService(IOptions<ApiOptions> options, IHttpClientWrapper httpClient)
+        {
+            _options = options;
+            _httpClient = httpClient;
+        }
+
+        public async Task<Response<T>> Fetch<T>(string url)
+        {
+            var response = await _httpClient.GetAsync(url).ConfigureAwait(false);
+
+            return ParseResponse<T>(response);
+        }
+
+        public Response<T> ParseResponse<T>(HttpResponseMessage responseMessage)
+        {
+            var response = new Response<T>();
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var result = responseMessage.Content.ReadAsStringAsync().Result;
+
+                response.message = result;
+            }
+
+            return response;
+        }
+    }
+
+    public interface IDataService
+    {
+        public Task<Response<T>> Fetch<T>(string url);
+
+        public Response<T> ParseResponse<T>(HttpResponseMessage responseMessage);
+    }
+}
