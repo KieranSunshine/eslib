99b77709ffad2084122f7c50c19a3b0d97c4a126
Kieran Sunshine <kieransunshine@hotmail.co.uk>
2021-01-03 02:35:44 +0000

    Created Alliance model

---
+++ eslib/Models/Alliance.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System;
+using System.Text.Json.Serialization;
+
+namespace eslib.Models
+{
+    public class Alliance
+    {
+        [JsonPropertyName("creator_corporation_id")]
+        public int CreatorCorporationId { get; set; }
+
+        [JsonPropertyName("creator_id")]
+        public int CreatorId { get; set; }
+
+        [JsonPropertyName("date_founded")]
+        public DateTime DateFounded { get; set; }
+
+        [JsonPropertyName("executor_corporation_id")]
+        public int ExecutorCorporationId { get; set; }
+
+        [JsonPropertyName("faction_id")]
+        public int FactionId { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("ticker")]
+        public string Ticker { get; set; }
+
+    }
+}
