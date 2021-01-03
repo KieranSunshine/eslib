37bb3f89659eb5822436509486c1db511f41ee9e
Kieran Sunshine <kieransunshine@hotmail.co.uk>
2021-01-03 00:25:47 +0000

    Renamed Alliances file to Alliance. Implemented GetAllianceIds method and releveant test

---
+++ eslib/Endpoints/Alliance.cs
@@ -0,0 +1,23 @@
+﻿using eslib.Services;
+
+namespace eslib.Endpoints
+{
+    public class Alliance
+    {
+        private readonly IDataService _dataService;
+        private readonly string endpoint = "alliances";
+
+        public Alliance(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        public int[] GetAllianceIds()
+        {            
+            var url = _dataService.GenerateUrl(endpoint);
+            var result = _dataService.Fetch<int[]>(url).Result;
+
+            return result.data;
+        }
+    }
+}
---
+++ eslib_units/Endpoints/Alliance.cs
@@ -0,0 +1,30 @@
+﻿using eslib.Endpoints;
+using eslib.Models;
+using eslib.Services;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace eslib_units.Endpoints
+{
+    public class AllianceTests
+    {
+        [Test]
+        public void GetAllianceIds()
+        {
+            var mock = new Mock<IDataService>();
+            var mockResponse = Task.FromResult(new Response<int[]>() { data = new int[100] });
+
+            mock.Setup(m => m.GenerateUrl(It.IsAny<string>()))
+                .Returns("somestring");
+
+            mock.Setup(m => m.Fetch<int[]>(It.IsAny<string>()))
+                .Returns(mockResponse);
+
+            var allianceEndpoint = new Alliance(mock.Object);
+            var result = allianceEndpoint.GetAllianceIds();
+
+            Assert.AreEqual(result, mockResponse.Result.data);
+        }
+    }
+}
