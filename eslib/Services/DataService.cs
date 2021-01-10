5eebb3983c179a2f8e921c218b01c45a15348ed5
Kieran Sunshine <kieransunshine@hotmail.co.uk>
2021-01-10 14:21:33 +0000

    Added Post method to DataService

--- eslib/Services/DataService.cs
+++ eslib/Services/DataService.cs
@@ -2,8 +2,6 @@
 using eslib.Models;
 using eslib.Services.Handlers;
 using Microsoft.Extensions.Options;
-using System;
-using System.Net;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -37,6 +35,15 @@ namespace eslib.Services
             return _responseHandler.Parse<T>(response);
         }
 
+        public async Task<Response<T>> Post<T>(string url, object data)
+        {                         
+            var httpContent = new StringContent(JsonSerializer.Serialize(data));
+
+            var response = await _httpClient.PostAsync(url, httpContent).ConfigureAwait(false);
+
+            return _responseHandler.Parse<T>(response);
+        }
+
         public string GenerateUrl(params string[] parameters)
         {
             var url = $"{Constants.apiUrl}/{string.Join("/", parameters)}";
@@ -47,7 +54,9 @@ namespace eslib.Services
 
     public interface IDataService
     {
-        public Task<Response<T>> Get<T>(string url);        
+        public Task<Response<T>> Get<T>(string url);
+
+        public Task<Response<T>> Post<T>(string url, object data);
 
         public string GenerateUrl(params string[] parameters);        
     }
