59416a9de644b082c45745caafc8c886b3046b98
Kieran Sunshine <kieransunshine@hotmail.co.uk>
2021-01-10 15:15:19 +0000

    Created Asset model

---
+++ eslib/Models/Asset.cs
@@ -0,0 +1,31 @@
+using System.Text.Json.Serialization;
+
+namespace eslib.Models
+{
+    public class Asset
+    {
+        [JsonPropertyName("is_blueprint_copy")]
+        public bool? IsBlueprintCopy { get; set; }
+
+        [JsonPropertyName("is_singleton")]
+        public bool IsSingleton { get; set; }
+
+        [JsonPropertyName("item_id")]
+        public long ItemId { get; set; }
+
+        [JsonPropertyName("location_flag")]
+        public string LocationFlag { get; set; }
+
+        [JsonPropertyName("location_id")]
+        public long LocationId { get; set; }
+
+        [JsonPropertyName("location_type")]
+        public string LocationType { get; set; }
+
+        [JsonPropertyName("quantity")]
+        public int Quantity { get; set; }
+
+        [JsonPropertyName("type_id")]
+        public int TypeId { get; set; }
+    }
+}
