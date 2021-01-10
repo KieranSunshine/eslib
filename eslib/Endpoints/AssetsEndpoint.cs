a03e0d2d95fbc4b4786a79876f266bd02e7aec67
Kieran Sunshine <kieransunshine@hotmail.co.uk>
2021-01-10 15:15:35 +0000

    Added basic implementation to AssetsEndpoint

--- eslib/Endpoints/AssetsEndpoint.cs
+++ eslib/Endpoints/AssetsEndpoint.cs
@@ -1,7 +1,50 @@
+using eslib.Models;
+using eslib.Services;
+using System.Collections;
+
 namespace eslib.Endpoints
 {
     public class AssetsEndpoint
     {
+        protected readonly IDataService _dataService;
+
+        public AssetsEndpoint(IDataService dataService)
+        {
+            _dataService = dataService;
+
+            Characters = new AssetOwner(this, "characters");
+            Corporations = new AssetOwner(this, "corporations");
+        }
+
+        // The same endpoint methods exist for both Characters and Corporations.
+        public IAssetOwner Characters;
+        public IAssetOwner Corporations;
+
+
+        // Define the reusable logic for the different Asset Owners.
+        private class AssetOwner : IAssetOwner
+        {            
+            private AssetsEndpoint _parent;
+            private string _ownerType;
+
+            public AssetOwner(AssetsEndpoint parent, string ownerType)
+            {
+                _parent = parent;
+                _ownerType = ownerType;
+            }
+
+            public Asset[] GetAssets(int id)
+            {
+                var url = _parent._dataService.GenerateUrl(_ownerType, id.ToString(), "assets");
+                var result = _parent._dataService.Get<Asset[]>(url).Result;
+
+                return result.data;
+            }
+        }
         
+        public interface IAssetOwner
+        {
+            public Asset[] GetAssets(int id);
+        }
     }
 }
