2dede2d2671e907f77095b2de3c3a3391c01ee62
Kieran <kieransunshine@hotmail.co.uk>
2021-01-24 19:29:39 +0000

    Added ApiVersions data to Constants file

--- eslib/Constants.cs
+++ eslib/Constants.cs
@@ -3,5 +3,12 @@
     internal static class Constants
     {
         public const string ApiUrl = "https://esi.evetech.net";
+
+        internal class ApiVersions
+        {
+            public const string Latest = "latest";
+            public const string Dev = "dev";
+            public const string Legacy = "legacy";
+        }
     }
 }
