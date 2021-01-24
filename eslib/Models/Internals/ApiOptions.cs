c63056fa15805f2f8fd149b6e2cf298914337d39
Kieran <kieransunshine@hotmail.co.uk>
2021-01-24 19:30:09 +0000

    Added properties to ApiOptions class

--- eslib/Models/Internals/ApiOptions.cs
+++ eslib/Models/Internals/ApiOptions.cs
@@ -3,5 +3,13 @@ namespace eslib.Models.Internals
 {
     public class ApiOptions
     {
+        public ApiOptions()
+        {
+            DataSource = "tranquility";
+            Version = Constants.ApiVersions.Latest;
+        }
+        
+        public string DataSource { get; }
+        public string Version { get; set; }
     }
 }
