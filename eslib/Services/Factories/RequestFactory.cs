5e4c4cacb87999cd378638ec7f26f05b2f1bd71d
Kieran <kieransunshine@hotmail.co.uk>
2021-01-24 19:31:09 +0000

    Created RequestFactory

---
+++ eslib/Services/Factories/RequestFactory.cs
@@ -0,0 +1,32 @@
+﻿using eslib.Models.Internals;
+
+namespace eslib.Services.Factories
+{
+    public class RequestFactory : IRequestFactory
+    {
+        private readonly ApiOptions _options;
+
+        public RequestFactory(ApiOptions options)
+        {
+            _options = options;
+        }
+
+        public Request Create()
+        {
+            var request = new Request()
+                .AddPaths(_options.Version)
+                .AddQuery("datasource", _options.DataSource);
+
+            return request;
+        }
+    }
+    
+    public interface IRequestFactory
+    {
+        /// <summary>
+        /// Creates an appropriately created Request
+        /// </summary>
+        /// <returns>The request</returns>
+        public Request Create();
+    }
+}
