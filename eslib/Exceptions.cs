efa02ee5dddea1a25183d20b72574d2cb9a2b507
Kieran <kieransunshine@hotmail.co.uk>
2021-01-30 23:19:01 +0000

    Created basic conversion exception

---
+++ eslib/Exceptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace eslib
+{
+    public class ConversionException : ApplicationException
+    {
+        public ConversionException(string message)
+            : base(message) { }
+
+        public ConversionException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
