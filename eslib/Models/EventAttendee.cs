2002e91c9397f872559e02890fee6dcbe7c6dea0
Kieran <kieransunshine@hotmail.co.uk>
2021-02-01 19:31:33 +0000

    Created EventAttendee model

---
+++ eslib/Models/EventAttendee.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace eslib.Models
+{
+    public class EventAttendee
+    {
+        [JsonPropertyName("character_id")] 
+        public int? CharacterId { get; set; }
+
+        [JsonPropertyName("event_response")] 
+        public Enums.Calendar.EventResponses Response { get; set; }
+    }
+}
