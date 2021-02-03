eeabe50d1b67c99c5fa4e6c2c82b8f3238196fb3
Kieran <kieransunshine@hotmail.co.uk>
2021-02-03 20:10:47 +0000

    Added CalendarEndpoint to ESI object

--- eslib/Esi.cs
+++ eslib/Esi.cs
@@ -14,11 +14,13 @@ namespace eslib
             Alliance = new AllianceEndpoint(options);
             Assets = new AssetsEndpoint(options);
             Bookmarks = new BookmarksEndpoint(options);
+            Calendar = new CalendarEndpoint(options);
         }
 
         public MetaEndpoint Meta { get; }
         public AllianceEndpoint Alliance { get; }
         public AssetsEndpoint Assets { get; }
         public BookmarksEndpoint Bookmarks { get; }
+        public CalendarEndpoint Calendar { get;  }
     }
 }
