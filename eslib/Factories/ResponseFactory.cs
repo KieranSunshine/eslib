3c20bed9743fe18b2205efd241c310edbf76ea1c
Kieran Sunshine <kieransunshine@hotmail.co.uk>
2021-05-04 20:36:38 +0100

    Tidied up ResponseFactory

--- eslib/Factories/ResponseFactory.cs
+++ eslib/Factories/ResponseFactory.cs
@@ -10,13 +10,10 @@ namespace Eslib.Factories
     {
         public EsiResponse<T> Create<T>(HttpResponseMessage responseMessage) where T : class
         {
-            EsiResponse<T> response;
-
-            // TODO: Convert method to async and await here.
             var status = responseMessage.StatusCode;
             var content = responseMessage.Content.ReadAsStringAsync().Result;
 
-            response = status switch {
+            var response = status switch {
                 HttpStatusCode.OK or
                 HttpStatusCode.NotModified or
                 HttpStatusCode.NoContent => ProcessResponse<T>(status, content),
@@ -30,12 +27,10 @@ namespace Eslib.Factories
         {
             if (string.IsNullOrEmpty(content))
                 return new EsiResponse<T>(statusCode);
-
-            // If data type is string, do not deserialize...
+            
             if (typeof(T) == typeof(string))
                 return new EsiResponse<T>(statusCode, content);
             
-            // Attempt to deserialize...
             T? data;
             try
             {
@@ -45,8 +40,7 @@ namespace Eslib.Factories
             {
                 throw new FormatException("Unable to deserialize the response into the given type", e);
             }
-
-            // Double check for null value.
+            
             if (data is null)
                 throw new ConversionException("Converted data cannot be null");
             
@@ -55,13 +49,9 @@ namespace Eslib.Factories
 
         private static EsiResponse<T> ProcessError<T>(HttpStatusCode statusCode, string content)
         {
-            // If no content received, return a generic response...
             if (string.IsNullOrEmpty(content))
-            {
                 return new EsiResponse<T>(statusCode);
-            }
-
-            // Attempt to deserialize the error...
+            
             Error? error;
             try
             {
@@ -69,15 +59,12 @@ namespace Eslib.Factories
             }
             catch (Exception e)
             {
-                throw new FormatException("Unable to deserialize the response error", e);
-            }
-
-            // Double check for null value.
-            if (error is null)
-            {
-                throw new ConversionException("Error data cannot be null");
+                throw new FormatException("Unable to deserialize the response", e);
             }
             
+            if (error is null)
+                throw new ConversionException("Error data cannot be null");
+
             return new EsiResponse<T>(statusCode, error);
         }
     }
